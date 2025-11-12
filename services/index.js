81a0ddb43b2d50e6fcd97b38081cc935ee5f5350
ahemd hamdy <ahmedhamdyz22435@gmail.com>
2025-11-12 14:20:34 +0200

    Update index.js

--- services/index.js
+++ services/index.js
@@ -3,22 +3,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { clerkMiddleware, requireAuth } from "@clerk/express"; // ممكن تحتاج requireAuth
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index"
-dotenv.config();
+import { inngest, functions } from "./inngest/inngest.js"; // 🚨🚨 أضف .js هنا 🚨🚨dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth-protected', clerkMiddleware(), (req, res) => {
-    // هنا الكود اللي محتاج يكون اليوزر عامل login
-    res.json({ message: "Authenticated user data", userId: req.auth?.userId });
+app.use("/api/auth-protected", clerkMiddleware(), (req, res) => {
+  res.json({ message: "Authenticated user data", userId: req.auth?.userId });
 });
- app.get('/',(req,res)=>{
-    res.send("Hello World")
- })
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
